736a94bd37fadf289cad20f8a06d11e594a0b3db
dourduck <dourduck@gmail.com>
2026-07-22 11:25:13 -0600

    removed game.c, cleaned up build.lua

--- src/game/game.h
+++ src/game/game.h
@@ -1,6 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "raylib.h"
+
 typedef int ExitKey;
 
 typedef struct {
@@ -12,8 +14,23 @@ typedef struct {
   int flags;
 } Environment;
 
-Environment Environment_CreateDefault();
-void Environment_InitWindow(Environment *env);
+static Environment Environment_CreateDefault() {
+  return (Environment){
+      .width = 800,
+      .height = 600,
+      .title = "Game",
+      .target_fps = 60,
+      .exit_key = KEY_Q,
+      .flags = 0,
+  };
+}
+
+static void Environment_InitWindow(Environment *env) {
+  SetConfigFlags(env->flags);
+  InitWindow(env->width, env->height, env->title);
+  SetTargetFPS(env->target_fps);
+  SetExitKey(env->exit_key);
+}
 
 #endif
 
